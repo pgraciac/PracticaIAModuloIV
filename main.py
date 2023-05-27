0999b5d2fff7568e9d4e56f7682b19dfb86b7d71
pgraciac <pgraciacorrea@gmail.com>
2023-05-27 19:53:31 +0200

    Ejercicio 1 y 2b done

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-
 import numpy as np
 import pandas as pd
 
@@ -15,79 +13,68 @@ def attribute_entropy(df, attribute):
     uniq_vals = df[attribute].unique()
     ent = 0
     for val in uniq_vals:
-        subdf = df[df[attribute] == val]['EsVenenosa']
+        subdf = df[df[attribute] == val][df.columns[-1]]
         pond = len(subdf) / len(df)
         ent += pond * entropy(subdf)
     return ent
 
 
-def best_attribute(df):
+def best_attribute(df, global_entropy):
     best_gain = 0
     best_attr = ''
     for key in df.iloc[:, :-1].keys():
-        key_gain = gain(df, key)
+        key_gain = gain(df, key, global_entropy)
         if key_gain > best_gain:
             best_gain = key_gain
             best_attr = key
     return best_attr, best_gain
 
 
-def gain(df, attribute):
-    return entropy(df['EsVenenosa']) - attribute_entropy(df, attribute)
+def gain(df, attribute, global_entropy):
+    res = global_entropy - attribute_entropy(df, attribute)
+    #print(global_entropy, "-", attribute_entropy(df, attribute), "=", res)
+    return res
 
-
-# def ID3(df, original_df, features):
-#     unique_vals = np.unique(df['EsVenenosa'])
-#     if len(unique_vals) == 1:
-#         return unique_vals[0]
-#
-#     elif len(features) == 0:
-#         return np.argmax(np.unique(original_df['EsVenenosa'], return_counts=True)[1])
-#
-#     else:
-#         best_feature, gain = best_attribute(df)
-#         features = [i for i in features if i != best_feature]
-#         tree = {best_feature: {}}
-#
-#         for value in np.unique(df[best_feature]):
-#             sub_data = df.where(df[best_feature] == value).dropna()
-#             subtree = ID3(sub_data, df, features)
-#             tree[best_feature][value] = subtree
-#
-#         return tree
-
-def ID3(df):
-    unique_vals = np.unique(df['EsVenenosa'])
+def ID3(df, global_entropy):
+    unique_vals = np.unique(df[df.columns[-1]])
     if len(unique_vals) == 1:
+        #print("Todos iguales", unique_vals)
+        #print(df)
         return unique_vals[0]
     elif len(df.keys()) <= 1:
-        return np.argmax(np.unique(df['EsVenenosa'], return_counts=True)[1])
+        # print("Último atributo")
+        # print(df)
+        return np.argmax(np.unique(df[df.columns[-1]], return_counts=True)[1])
     if len(df.keys()) > 1:
-        best_attr = best_attribute(df)[0]
+        # print(df)
+        best_attr, best_gain = best_attribute(df, global_entropy)
+        # print("Mejor atributo y ganancia es", best_attr, best_gain)
         tree = {best_attr: {}}
         for val in np.unique(df[best_attr]):
             sub_data = df.where(df[best_attr] == val).dropna().drop([best_attr], axis=1)
-            subtree = ID3(sub_data)
+            subtree = ID3(sub_data, global_entropy)
             tree[best_attr][val] = subtree
 
         return tree
-    
+
+
 def predict(observation, tree):
     for nodes in tree.keys():
         value = observation[nodes]
         tree = tree[nodes][value]
         prediction = 0
-        
+
         if type(tree) is dict:
             prediction = predict(observation, tree)
         else:
             prediction = tree
             break
-            
+
     return prediction
 
 
 if __name__ == '__main__':
+    #Ejercicio 1
     df_train = pd.DataFrame({'PesaMucho': [0, 0, 1, 1, 0, 0, 0, 1],
                              'EsMaloliente': [0, 0, 1, 0, 1, 0, 0, 1],
                              'EsConManchas': [0, 1, 0, 0, 1, 1, 0, 0],
@@ -101,13 +88,22 @@ if __name__ == '__main__':
                            index=['U', 'V', 'W'])
     EsVenenosa_entropy = entropy(df_train['EsVenenosa'])
     print(f"1.a)\nLa entropia de EsVenenosa es: {EsVenenosa_entropy}")
-    print(attribute_entropy(df_train, 'EsSuave'))
-    print(best_attribute(df_train))
+    print(f"1.c)\nLa ganancia de EsSuave es: {gain(df_train, 'EsSuave', EsVenenosa_entropy)}")
 
-    # features = df_train.columns[:-1].tolist()
-    tree = ID3(df_train)
-    print(tree)
+    tree = ID3(df_train, EsVenenosa_entropy)
+    print(f"1.d)\nEl arbol generado por el algoritmo es: {tree}")
     predictions = df_test.apply(predict, axis=1, args=(tree,))
-    print(predictions)
+    print(f"1.e)\nLas predicciones para las setas U, V y W son:\n{predictions}")
 
-   
+    # EJERCICIO 2
+    df_train = pd.DataFrame({'x1': [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+                             'x2': [0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1],
+                             'x3': [1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1],
+                             'x4': [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1],
+                             'x5': [0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0],
+                             'x6': [0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0],
+                             'T': [1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0]},
+                            index=['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'A11', 'A12', 'A13',
+                                   'A14'])
+    T_entropy = entropy(df_train['T'])
+    print(f"1.b)\nEl arbol generado por el algoritmo es: {ID3(df_train, T_entropy)}")
